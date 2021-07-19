67e378aabdfe3963d50b857068a18b12369f29d2
rens243 <17591560+rens243@users.noreply.github.com>
2021-07-19 10:34:58 +0200

    catch

--- app/Services/HousesService.php
+++ app/Services/HousesService.php
@@ -84,50 +84,55 @@ class HousesService
                 );
 
                 // Get all houses
-                $crawler
-                    ->filter($estate->selector_all)
-                    ->filter($estate->selector_each)
-                    ->each(function (Crawler $node) use ($estate, &$houses) {
-                        $estate_id = $estate->id;
-                        \Log::debug('Got id');
+                try {
+                    $crawler
+                        ->filter($estate->selector_all)
+                        ->filter($estate->selector_each)
+                        ->each(function (Crawler $node) use ($estate, &$houses) {
+                            $estate_id = $estate->id;
+                            \Log::debug('Got id');
 
-                        $name = $node->filter($estate->selector_name)->text();
-                        \Log::debug('Got name '.$name);
+                            $name = $node->filter($estate->selector_name)->text();
+                            \Log::debug('Got name '.$name);
 
-                        // Create realiable house link
-                        $link = $node->filter($estate->selector_link)->first()->attr('href');
-                        $link = UriResolver::resolve($link, $estate->url);
+                            // Create realiable house link
+                            $link = $node->filter($estate->selector_link)->first()->attr('href');
+                            $link = UriResolver::resolve($link, $estate->url);
 
-                        \Log::debug('Got link '.$link);
+                            \Log::debug('Got link '.$link);
 
-                        // Try <img> and style="background-image"
-                        try {
-                            // Try normal image
-                            $photo = $node->filter($estate->selector_photo)->image()->getUri();
-                        } catch (\Exception $e) {
-                            // Try style
-                            $photoStyle = $node->filter($estate->selector_photo)->attr("style");
-                            preg_match('/(?<=url\().+?(?=\))/', $photoStyle, $matches);
-                            $photo = UriResolver::resolve($matches[0], $estate->url);
-                        }
-                        \Log::debug('Got photo '.$photo);
+                            // Try <img> and style="background-image"
+                            try {
+                                // Try normal image
+                                $photo = $node->filter($estate->selector_photo)->image()->getUri();
+                            } catch (\Exception $e) {
+                                // Try style
+                                $photoStyle = $node->filter($estate->selector_photo)->attr("style");
+                                preg_match('/(?<=url\().+?(?=\))/', $photoStyle, $matches);
+                                $photo = UriResolver::resolve($matches[0], $estate->url);
+                            }
+                            \Log::debug('Got photo '.$photo);
 
-                        $description = implode(
-                            '\n',
-                            $node->filter($estate->selector_description)
-                                ->each(fn($node) => $node->text())
-                        );
-                        \Log::debug('Got description');
+                            $description = implode(
+                                '\n',
+                                $node->filter($estate->selector_description)
+                                    ->each(fn($node) => $node->text())
+                            );
+                            \Log::debug('Got description');
 
-                        $price = $node->filter($estate->selector_price)->text();
-                        \Log::debug('Got price');
+                            $price = $node->filter($estate->selector_price)->text();
+                            \Log::debug('Got price');
 
-                        $raw = $node->html();
-                        \Log::debug('Got raw');
+                            $raw = $node->html();
+                            \Log::debug('Got raw');
 
-                        // Push house values to array
-                        array_push($houses, compact('name', 'estate_id', 'description', 'photo', 'price', 'link', 'raw'));
-                    });
+                            // Push house values to array
+                            array_push($houses, compact('name', 'estate_id', 'description', 'photo', 'price', 'link', 'raw'));
+                        });
+                } catch (\Exception $e) {
+                    \Log::error((string) $e);
+                }
+
             },
             'rejected' => function (RequestException $reason, $index) {
                 // this is delivered each failed request
